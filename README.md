65d0c79dd6c76aa0c63f878460c11e8fff62d374
TurtleWolfe <jonpohlner@gmail.com>
2018-04-12 13:43:01 +0000

    added materialize & dropshadow

--- README.md
+++ README.md
@@ -3,6 +3,7 @@
 npm install -g create-react-app
 create-react-app css-grid-template
 cd css-grid-template
+npm install react-materialize --save
 ```  
 ```
 cd css-grid-template
--- css-grid-template/src/App.js
+++ css-grid-template/src/App.js
@@ -7,28 +7,28 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+          
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <div className="container">
-            <div><img src="img/normal1.jpg"/></div>
-            <div className="vertical"><img src="img/vertical1.jpg"/></div>
-            <div className="horizontal"><img src="img/horizontal1.jpg"/></div>
-            <div><img src="img/normal2.jpg"/></div>
-            <div><img src="img/normal3.jpg"/></div>
-            <div className="big"><img src="img/big1.jpg"/></div>
-            <div><img src="img/normal4.jpg"/></div>
-            <div className="vertical"><img src="img/vertical2.jpg"/></div>
-            <div><img src="img/normal5.jpg"/></div>
-            <div className="horizontal"><img src="img/horizontal2.jpg"/></div>
-            <div><img src="img/normal6.jpg"/></div>
-            <div className="big"><img src="img/big2.jpg"/></div>
-            <div><img src="img/normal7.jpg"/></div>
-            <div className="horizontal"><img src="img/horizontal3.jpg"/></div>
-            <div><img src="img/normal8.jpg"/></div>
-            <div className="big"><img src="img/big3.jpg"/></div>
-            <div><img src="img/normal9.jpg"/></div>
-            <div className="vertical"><img src="img/vertical3.jpg"/></div>
+            <div className="z-depth-2" alt="image wordings"><img src="img/normal1.jpg"/></div>
+            <div className="vertical z-depth-2" alt="image wordings"><img src="img/vertical1.jpg"/></div>
+            <div className="horizontal z-depth-2" alt="image wordings"><img src="img/horizontal1.jpg"/></div>
+            <div className="z-depth-2" alt="image wordings"><img src="img/normal2.jpg"/></div>
+            <div className="z-depth-2" alt="image wordings"><img src={logo} className="App-logo z-depth-2" alt="logo" /></div>
+            <div className="big z-depth-2" alt="image wordings"><img src="img/big1.jpg"/></div>
+            <div className="z-depth-2" alt="image wordings"><img src="img/normal4.jpg"/></div>
+            <div className="vertical z-depth-2" alt="image wordings"><img src="img/vertical2.jpg"/></div>
+            <div className="z-depth-2" alt="image wordings"><img src="img/normal5.jpg"/></div>
+            <div className="horizontal z-depth-2" alt="image wordings"><img src="img/horizontal2.jpg"/></div>
+            <div className="z-depth-2" alt="image wordings"><img src="img/normal6.jpg"/></div>
+            <div className="big z-depth-2" alt="image wordings"><img src="img/big2.jpg"/></div>
+            <div className="z-depth-2" alt="image wordings"><img src="img/normal7.jpg"/></div>
+            <div className="horizontal z-depth-2" alt="image wordings"><img src="img/horizontal3.jpg"/></div>
+            <div className="z-depth-2" alt="image wordings"><img src="img/normal8.jpg"/></div>
+            <div className="big z-depth-2" alt="image wordings"><img src="img/big3.jpg"/></div>
+            <div className="z-depth-2" alt="image wordings"><img src="img/normal9.jpg"/></div>
+            <div className="vertical z-depth-2" alt="image wordings"><img src="img/vertical3.jpg"/></div>
         </div>
       </div>
     );
