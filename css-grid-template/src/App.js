1b6f61ee3debff88c845524afd3beaab8de0631a
TurtleWolfe <jonpohlner@gmail.com>
2018-04-12 16:17:56 +0000

    10 images

--- css-grid-template/src/App.js
+++ css-grid-template/src/App.js
@@ -13,10 +13,10 @@ class App extends Component {
         <div className="container">
             <div className="z-depth-2" alt="image wordings"><img src="img/normal1.jpg"/></div>
             <div className="vertical z-depth-2" alt="image wordings"><img src="img/vertical1.jpg"/></div>
-            <div className="horizontal z-depth-2" alt="image wordings"><img src="img/horizontal1.jpg"/></div>
+            <div className="horizontal z-depth-2" alt="image wordings"><img src="img/cssGRID/mysql.png"/></div>
             <div className="z-depth-2" alt="image wordings"><img src="img/normal2.jpg"/></div>
             <div className="z-depth-2" alt="image wordings"><img src={logo} className="App-logo" alt="logo" /></div>
-            <div className="big z-depth-2" alt="image wordings"><img src="img/big1.jpg"/></div>
+            <div className="big z-depth-2" alt="image wordings"><img src="img/cssGRID/images.jpg"/></div>
             <div className="z-depth-2" alt="image wordings"><img src="img/normal4.jpg"/></div>
             <div className="social-link-list vertical z-depth-2" alt="image wordings">                    <ul>
                         <li><a href="https://codepen.io/TurtleWolf/"><i className="fa fa-codepen" aria-hidden="true"></i> CodePen</a>
@@ -34,14 +34,14 @@ class App extends Component {
                     </ul></div>
             <div className="z-depth-2" alt="image wordings"><img src="img/normal5.jpg"/></div>
             <div className="horizontal z-depth-2" alt="image wordings"><img src="img/horizontal2.jpg"/></div>
-            <div className="z-depth-2" alt="image wordings"><img src="img/normal6.jpg"/></div>
+            <div className="z-depth-2" alt="image wordings"><img src="img/cssGRID/git.png"/></div>
             <div className="big z-depth-2" alt="image wordings"><img src="img/big2.jpg"/></div>
-            <div className="z-depth-2" alt="image wordings"><img src="img/normal7.jpg"/></div>
-            <div className="horizontal z-depth-2" alt="image wordings"><img src="img/horizontal3.jpg"/></div>
+            <div className="z-depth-2" alt="image wordings"><img src="img/cssGRID/nodejs-logo.png"/></div>
+            <div className="horizontal z-depth-2" alt="image wordings"><img src="img/cssGRID/php.png"/></div>
             <div className="z-depth-2" alt="image wordings"><img src="img/normal8.jpg"/></div>
-            <div className="big z-depth-2" alt="image wordings"><img src="img/big3.jpg"/></div>
-            <div className="z-depth-2" alt="image wordings"><img src="img/normal9.jpg"/></div>
-            <div className="vertical z-depth-2" alt="image wordings"><img src="img/vertical3.jpg"/></div>
+            <div className="big z-depth-2" alt="image wordings"><img src="img/cssGRID/cssgrid.png"/></div>
+            <div className="z-depth-2" alt="image wordings"><img src="img/cssGRID/matrix.gif"/></div>
+            <div className="vertical z-depth-2" alt="image wordings"><img src="img/cssGRID/html5.jpg"/></div>
         </div>
       </div>
     );
