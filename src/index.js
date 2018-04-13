15b593ca44597ff23a462be3d8eaee27f90dfce7
TurtleWolfe <jonpohlner@gmail.com>
2018-04-13 15:24:41 +0000

    cleaned code, passing all tests

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Button, Card, Row, Col } from 'react-materialize';
+//import { Button, Card, Row, Col } from 'react-materialize';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
